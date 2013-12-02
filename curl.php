6cc64f5cbf6c400d6450dd232ff6a6e5878bf47c
sean lyons <sean@hexault.com>
2013-12-02 01:42:35 -0600

    merged process' functionality into curl, removing the need for intermediary files.

--- curl.php
+++ curl.php
@@ -10,10 +10,47 @@ while (TRUE) {
     $head = curl_exec($ch);
     curl_close($ch);
     
-    file_put_contents('askreddit_'.time().'.html', $head);
+    go( $head );
 
     for($i = 0; $i <= 1800; $i++) {
         echo "$i\n";
 		sleep(1);
     }
+}
+
+function go ( $input ) {
+	$dom = new DomDocument();
+	@$dom->loadHTML($input);
+	$finder = new DomXPath($dom);
+
+	$classname="title";
+	$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), '$classname')]");
+// print_r($nodes);
+
+	for ($i = 0; $i < $nodes->length; $i++) {
+		$nv = $nodes->item($i)->nodeValue;
+		if (strpos($nv, 'self.AskReddit') === FALSE) {
+			$qa[] = $nv;
+		}
+	}
+// print_r($qa);
+	for ($i = 0; $i <= 4; $i++) {
+		array_pop($qa);
+	}
+	unset($qa[0], $qa[1], $qa[2]);
+// print_r($qa);
+    foreach ($qa as $k => $v) {
+        $fixed[sha1($v)] = $v;
+    }    
+    
+    $master_doc = file('master_doc.txt', FILE_IGNORE_NEW_LINES);
+    foreach($master_doc as $k => $v) {
+        $md[sha1($v)] = $v;
+    }
+print_r($md);
+print_r($fixed);
+    $merged = array_merge($md, $fixed);
+	$string = implode("\n", $merged);
+	$string .= "\n\n";
+	file_put_contents('master_doc.txt', $string);
 }
